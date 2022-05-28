ed4649b547dc74840479b55abb753e7821e4699a
angelo28b <101931022+angelo28b@users.noreply.github.com>
2022-05-29 00:09:05 +0800

    first commit

---
+++ src/Collection.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import './Ccss.css';
+import nice from './nice.jpg';
+
+export default function Collection() {
+  return (
+<>
+
+<div class='head'>  
+
+<h1>this website is created by michael angelo billones of bsit2a</h1>
+</div>
+
+
+<div class='profile'>
+<img src={nice} height='400px' width='400px' alt="logo" />
+</div>
+
+<div class='head'>
+  
+  <h1>welcome to my landing page!</h1>
+
+</div>
+</>
+
+
+
+
+
+
+
+
+
+  )
+}
